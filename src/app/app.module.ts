20ff0b70b201e33684baab883db3c791ac72958f
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-04-15 19:52:36 +0300

    This will add the form, detials and main bosy components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainbodyComponent } from './mainbody/mainbody.component';
+import { FormComponent } from './form/form.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainbodyComponent,
+    FormComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
